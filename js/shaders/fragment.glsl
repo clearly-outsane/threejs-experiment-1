a7d13c4b326f23330b5c7d5b2dc60eeaec9156a3
clearly-outsane <1starwarrior516@gmail.com>
2021-06-22 03:08:58 +0530

    colors

--- js/shaders/fragment.glsl
+++ js/shaders/fragment.glsl
@@ -1,9 +1,9 @@
 varying float vNoise;
 
 void main(){
-    vec3 color1=vec3(1.,.2078,.8039);
-    vec3 color2=vec3(.349,0.,1.);
-    vec3 finalColor=mix(color1,color2,.5*(vNoise+1.));
+    vec3 color1=vec3(.043,.275,.549);
+    vec3 color2=vec3(.388,.204,.369);
+    vec3 finalColor=2.*mix(color1,color2,.5*(vNoise+1.));
     
     gl_FragColor=vec4(finalColor,1.);
 }
